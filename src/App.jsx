d9f126fb02af8fcae1e1e08f5b523e5c9a12a77c
lambangws21 <lambangws21@gmail.com>
2023-08-30 09:57:54 +0700

    sudah di upload ulang

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 /** @format */
-import "./App.css";
 import Ansietas from "./Tabel/Ansietas";
 import AnsietasIntervensi from "./Tabel/AnsietasIntervensi";
 import AnsietasLuaran from "./Tabel/AnsietasLuaran";
@@ -20,132 +19,133 @@ import RisikoInfeksi from "./Tabel/RisikoInfeksi";
 import RisikoInfeksiLuaran from "./Tabel/RisikoInfeksiLuraran";
 import RisikoInfekIntervensi from "./Tabel/RisikoInfekIntervensi";
 
-
 function App() {
   return (
     <>
-      <table className="w-full border-collapse ">
-        <thead>
-          <tr>
-            <th
-              className="
+      <div className="container mx-auto p-10">
+        <table className="w-full border-collapse  ">
+          <thead>
+            <tr>
+              <th
+                className="
           border
           bg-sky-100 
           p-1 shadow-md 
           leading-10">
-              Diagnosa Keperawatan
-            </th>
-            <th
-              className="
+                Diagnosa Keperawatan
+              </th>
+              <th
+                className="
           border
           bg-sky-100 
           p-1 shadow-md 
           leading-10">
-              Luaran Keperawatan
-            </th>
-            <th
-              className="
+                Luaran Keperawatan
+              </th>
+              <th
+                className="
           border
           bg-sky-100 
           p-1 shadow-md 
           leading-10">
-              Intervensi Keperawatan
-            </th>
-          </tr>
-        </thead>
-        <tbody className="text-left relative align-top leading-loose">
-          {/* Ansietas*/}
-          <tr>
-            <td className="flex-col items-center border-b-2 border-gray-300 p-2 h-72 ">
-              <Ansietas />
-              <Button />
-            </td>
-            <td className="flex-col items-center border-b-2 border-gray-300 h-72">
-              <AnsietasLuaran />
-              <Button />
-            </td>
-            <td className="flex-col items-center border-b-2 border-gray-300 h-72">
-              <AnsietasIntervensi />
-              <Button />
-            </td>
-          </tr>
-          {/* Risiko Perdarahan */}
-          <tr>
-            <td className="flex-col items-center border-b-2 border-gray-300 p-2 h-72 ">
-              <RisikoPerdarahan />
-              <Button />
-            </td>
-            <td className="flex-col items-center border-b-2 border-gray-300 h-72">
-              <RisPerdaLuaran />
-              <Button />
-            </td>
-            <td className="flex-col items-center border-b-2 border-gray-300 h-72">
-              <RisPerdaIntervensi />
-              <Button />
-            </td>
-          </tr>
-          {/* Nyeri Akut */}
-          <tr>
-            <td className="flex-col items-center border-b-2 border-gray-300 p-2 h-72">
-              <NyeriAkut />
-              <Button />
-            </td>
-            <td className="flex-col items-center border-b-2 border-gray-300 h-72">
-              <NyeriAkutLuaran />
-              <Button />
-            </td>
-            <td className="flex-col items-center border-b-2 border-gray-300 h-72">
-              <NyeriAkutIntervensi />
-              <Button />
-            </td>
-          </tr>
-          {/* Risiko Hiptermi */}
-          <tr>
-            <td className="flex-col items-center border-b-2 border-gray-300 p-2 h-72">
-              <RisikoHipotermi />
-              <Button />
-            </td>
-            <td className="flex-col items-center border-b-2 border-gray-300 h-72">
-              <RiskHipoLuaran />
-              <Button />
-            </td>
-            <td className="flex-col items-center border-b-2 border-gray-300 h-72">
-              <RisHipoIntervensi />
-              <Button />
-            </td>
-          </tr>
-          {/* Risiko Hiptermi */}
-          <tr>
-            <td className="flex-col items-center border-b-2 border-gray-300 p-2 h-72">
-              <RisikoJatuh />
-              <Button />
-            </td>
-            <td className="flex-col items-center border-b-2 border-gray-300 h-72">
-              <RisJatuhLuaran />
-              <Button />
-            </td>
-            <td className="flex-col items-center border-b-2 border-gray-300 h-72">
-              <RisJatuhIntervensi />
-              <Button />
-            </td>
-          </tr>
-          {/* Risiko Infeksi */}
-          <tr>
-            <td className="flex-col items-center border-b-2 border-gray-300 p-2 h-72">
-              <RisikoInfeksi />
-              <Button />
-            </td>
-            <td className="flex-col items-center border-b-2 border-gray-300 h-72">
-              <RisikoInfeksiLuaran />
-              <Button />
-            </td>
-            <td className="flex-col items-center border-b-2 border-gray-300 h-72">
-          <RisikoInfekIntervensi/>
-              <Button />
-            </td>
-          </tr>
-        </tbody>
-      </table>
+                Intervensi Keperawatan
+              </th>
+            </tr>
+          </thead>
+          <tbody className="text-left relative align-top leading-loose">
+            {/* Ansietas*/}
+            <tr>
+              <td className="flex-col items-center border-b-2 border-gray-300 p-2 h-72 ">
+                <Ansietas />
+                <Button />
+              </td>
+              <td className="flex-col items-center border-b-2 border-gray-300 h-72">
+                <AnsietasLuaran />
+                <Button />
+              </td>
+              <td className="flex-col items-center border-b-2 border-gray-300 h-72">
+                <AnsietasIntervensi />
+                <Button />
+              </td>
+            </tr>
+            {/* Risiko Perdarahan */}
+            <tr>
+              <td className="flex-col items-center border-b-2 border-gray-300 p-2 h-72 ">
+                <RisikoPerdarahan />
+                <Button />
+              </td>
+              <td className="flex-col items-center border-b-2 border-gray-300 h-72">
+                <RisPerdaLuaran />
+                <Button />
+              </td>
+              <td className="flex-col items-center border-b-2 border-gray-300 h-72">
+                <RisPerdaIntervensi />
+                <Button />
+              </td>
+            </tr>
+            {/* Nyeri Akut */}
+            <tr>
+              <td className="flex-col items-center border-b-2 border-gray-300 p-2 h-72">
+                <NyeriAkut />
+                <Button />
+              </td>
+              <td className="flex-col items-center border-b-2 border-gray-300 h-72">
+                <NyeriAkutLuaran />
+                <Button />
+              </td>
+              <td className="flex-col items-center border-b-2 border-gray-300 h-72">
+                <NyeriAkutIntervensi />
+                <Button />
+              </td>
+            </tr>
+            {/* Risiko Hiptermi */}
+            <tr>
+              <td className="flex-col items-center border-b-2 border-gray-300 p-2 h-72">
+                <RisikoHipotermi />
+                <Button />
+              </td>
+              <td className="flex-col items-center border-b-2 border-gray-300 h-72">
+                <RiskHipoLuaran />
+                <Button />
+              </td>
+              <td className="flex-col items-center border-b-2 border-gray-300 h-72">
+                <RisHipoIntervensi />
+                <Button />
+              </td>
+            </tr>
+            {/* Risiko Hiptermi */}
+            <tr>
+              <td className="flex-col items-center border-b-2 border-gray-300 p-2 h-72">
+                <RisikoJatuh />
+                <Button />
+              </td>
+              <td className="flex-col items-center border-b-2 border-gray-300 h-72">
+                <RisJatuhLuaran />
+                <Button />
+              </td>
+              <td className="flex-col items-center border-b-2 border-gray-300 h-72">
+                <RisJatuhIntervensi />
+                <Button />
+              </td>
+            </tr>
+            {/* Risiko Infeksi */}
+            <tr>
+              <td className="flex-col items-center border-b-2 border-gray-300 p-2 h-72">
+                <RisikoInfeksi />
+                <Button />
+              </td>
+              <td className="flex-col items-center border-b-2 border-gray-300 h-72">
+                <RisikoInfeksiLuaran />
+                <Button />
+              </td>
+              <td className="flex-col items-center border-b-2 border-gray-300 h-72">
+                <RisikoInfekIntervensi />
+                <Button />
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </>
   );
 }
