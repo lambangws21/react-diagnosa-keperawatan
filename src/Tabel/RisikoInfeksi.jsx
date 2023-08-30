91be2fc3f0d9d8c38cbec45b9d941910d33fd9bb
lambangws21 <lambangws21@gmail.com>
2023-08-30 12:08:24 +0700

    mengubah judul

--- src/Tabel/RisikoInfeksi.jsx
+++ src/Tabel/RisikoInfeksi.jsx
@@ -3,7 +3,7 @@ function RisikoInfeksi() {
   return (
     <div>
       <td className="py-2">
-        Risiko infeksi b/d Efek prosedur infasif, penyakit kronis (Deabetes),
+        Risiko infeksi b/d Efek prosedur infasif, penyakit kronis (Diabetes),
         ketidak adekuatan pertahanan tubuh (merokok, penurunan Hemoglobin)
       </td>
     </div>
